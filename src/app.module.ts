4df93d40347e61bd12bd4edde33486a4fbbee235
nhq.huy102 <nhq.huy102@gmail.com>
2025-09-16 15:22:40 +0000

    update connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,27 @@ import { SeatsModule } from './seats/seats.module';
 import { TripsModule } from './trips/trips.module';
 import { BusesModule } from './buses/buses.module';
 import { RoutesModule } from './routes/routes.module';
-import { RoutesModule } from './routes/routes.module';
-import { BusesModule } from './buses/buses.module';
-import { TripsModule } from './trips/trips.module';
-import { SeatsModule } from './seats/seats.module';
-import { BookingsModule } from './bookings/bookings.module';
-import { PaymentsModule } from './payments/payments.module';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, RoutesModule, BusesModule, TripsModule, SeatsModule, BookingsModule, PaymentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '103.82.24.7',
+      port: 3306,
+      username: 'mhirgrp_bus_tickets',
+      password: 'mhirgrp_bus_tickets',
+      database: 'mhirgrp_bus_tickets',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule, 
+    RoutesModule, 
+    BusesModule, 
+    TripsModule, 
+    SeatsModule, 
+    BookingsModule, 
+    PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
