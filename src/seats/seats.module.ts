9ffcb9a57ea09c7ba323f497e1b4a8a9dce4d5d1
nhq.huy102 <nhq.huy102@gmail.com>
2025-09-16 17:52:24 +0000

    update

--- src/seats/seats.module.ts
+++ src/seats/seats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeatsService } from './seats.service';
 import { SeatsController } from './seats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Seat } from './entities/seat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Seat])],
   controllers: [SeatsController],
   providers: [SeatsService],
 })
